edc0aaea88726242232f5531d139c148b0f1eba8
Jellepa <jelle.paulus@gmail.com>
2022-10-11 16:00:03 +0200

    updated webpack config

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(glb|gltf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.mp3$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.ttf$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
